99caad82fc31ac31f20c31edd52616a2238616fe
SirisakUnknowss <sirisak.unknowss@gmail.com>
2022-03-13 02:28:14 +0700

    implement html template

--- webApp/LabEquipmentLoanSystem/urls.py
+++ webApp/LabEquipmentLoanSystem/urls.py
@@ -24,4 +24,5 @@ from base import views as baseViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', baseViews.homepage, name='homepage'),
+    path('login', baseViews.login, name='loginApi'),
 ]
--- webApp/base/views.py
+++ webApp/base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'base/index.html')
+
+def login(request):
+    return render(request, 'base/login.html')
