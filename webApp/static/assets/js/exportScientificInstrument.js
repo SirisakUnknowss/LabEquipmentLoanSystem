2aeee2677d8a80937b55edd7d751be87a6f67f60
SirisakUnknowss <82793984+SirisakUnknowss@users.noreply.github.com>
2023-10-16 14:21:05 +0700

    export file

---
+++ webApp/static/assets/js/exportScientificInstrument.js
@@ -0,0 +1,28 @@
+var nameFile = ""
+async function loadContent(url="") {
+    fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      // the filename you want
+      
+      a.download = nameFile;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    })
+    .catch(() => alert("Error downloading the file!"));
+}
+
+async function requestContent(url="") {
+    loadContent(url)
+}
+const exportScientificInstrumentAllButton = document.querySelector("#exportScientificInstrumentAllButton")
+
+exportScientificInstrumentAllButton.addEventListener('click', (event) => {
+    nameFile = "scientificInstrumentAllData.xlsx"
+    requestContent(urlExportData + "?getData=")
+})
