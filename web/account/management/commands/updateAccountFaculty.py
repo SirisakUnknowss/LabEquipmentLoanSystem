135ce83be711719088c5e4e16cf89110c9706cae
Yaijino <sirisak.unknowss@gmail.com>
2024-06-24 15:27:03 +0700

    Update updateAccountFaculty.py

--- web/account/management/commands/updateAccountFaculty.py
+++ web/account/management/commands/updateAccountFaculty.py
@@ -8,19 +8,15 @@ from account.models import Account
 
 class Command(BaseCommand):
     
-    help = "Update prefix and levelClass in All Accounts."
+    help = "Update faculty in Other faculty of Accounts."
 
     def handle(self, *args, **options):
         try:
-            with open('./account/management/commands/output_data.json', 'r', encoding='utf-8') as f:
-                data_list = json.load(f)
-            for entry in data_list:
-                print(entry['prefix'])
-                print(entry['levelClass'])
-                account = Account.objects.get(id=entry['id'])
-                account.prefix = entry['prefix']
-                account.levelClass = entry['levelClass']
-                account.save(update_fields=["prefix", "levelClass"])
+            accounts = Account.objects.filter(faculty="Other")
+            for account in accounts:
+                print(account.faculty)
+                account.faculty = "other"
+                account.save(update_fields=["faculty"])
                 self.stdout.write(self.style.SUCCESS(f'{account.studentID} Update Complete!'))
             self.stdout.write(self.style.SUCCESS('Update All Complete!'))
         except Exception as error:
